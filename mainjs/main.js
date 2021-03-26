2e54fd94a978babdffe53b100fdcbf097d15c72c
cillacode <pwicyhits@gmail.com>
2021-03-26 15:22:38 +0100

    hotelproject

---
+++ mainjs/main.js
@@ -0,0 +1,19 @@
+    alert("welcome please fill the form");
+
+var mary = "mary";
+var email = "gmail";
+
+name = "mary";
+if (name === mary) {
+    alert("welcome mary");
+} else {
+    alert("i dont know you");
+}
+var mary = "green";
+name = "green";
+
+if (name === mary) {
+    alert("welcome mary");
+} else {
+    alert("i dont know you");
+}
